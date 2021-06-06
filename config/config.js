89f5aae3cf137afb49284bf0dc6beaaf18c0561d
ibradezip <68031655+ibradezip@users.noreply.github.com>
2021-06-06 15:33:16 +0200

    AroxiaRP

---
+++ config/config.js
@@ -0,0 +1,12 @@
+module.exports.options = {
+    token: 'Nzk4MjgxMjgwOTE5MzA2Mjcx.X_yvYA.OwtT3Gzc2jbbPWMROmriEtzjC7U',
+    topicChannels: [
+      '🎉･status'
+    ],
+    roles: ["Admin"],
+    servers: [
+        { name: 'Aroxia', url: 'http://147.189.171.123:30120', alias: 'NewYork' },
+        { name: 'Aroxia', url: 'http://127.0.0.1:30121:30120', alias: 'LosAngeles' }
+    ],
+    pollRate: 30,
+};
